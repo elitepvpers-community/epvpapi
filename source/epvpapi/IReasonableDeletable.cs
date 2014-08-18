1a8bb5b4e8fb2aefabaf03a8761e38c43a7090e9
Björn <mostey1337@hotmail.de>
2014-08-18 19:56:28 +0200

    Import from private repository

---
+++ source/epvpapi/IReasonableDeletable.cs
@@ -0,0 +1,22 @@
+﻿using epvpapi.Connection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epvpapi
+{
+    /// <summary>
+    /// Interface for objects that are deletable requiring a reason for the deletion
+    /// </summary>
+    public interface IReasonableDeletable
+    {
+        /// <summary>
+        /// Deletes the object using the given <c>Session</c>
+        /// </summary>
+        /// <param name="session"> Session that is used for sending the request </param>
+        /// <param name="reason"> Reason for the deletion </param>
+        void Delete(Session session, string reason);
+    }
+}
---
+++ source/epvpapi/IReportable.cs
@@ -0,0 +1,22 @@
+﻿using epvpapi.Connection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epvpapi
+{
+    /// <summary>
+    /// Interface implemented by objects that can be reported
+    /// </summary>
+    interface IReportable
+    {
+        /// <summary>
+        /// Reports the object 
+        /// </summary>
+        /// <param name="session"> Session that is used for sending the request </param>
+        /// <param name="reason"> Reason of the report </param>
+        void Report(Session session, string reason);
+    }
+}
