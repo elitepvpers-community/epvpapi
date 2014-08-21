357c3f2c020f9b9c85096bf32327f075679bc9ef
Mostey <mostey1337@hotmail.de>
2014-08-21 21:35:24 +0200

    Function for parsing an elitepvpers profile link

--- src/epvpapi/User.cs
+++ src/epvpapi/User.cs
@@ -175,5 +175,18 @@ namespace epvpapi
                 LastActivity = parsedDateTime; 
             }
         }
+
+        /// <summary>
+        /// Retrieves the profile ID and name from the given URL and converts them into a valid <c>User</c> object
+        /// </summary>
+        /// <param name="url"> URL being parsed </param>
+        /// <returns> Valid <c>User</c> object containing profile ID and name from the given URL </returns>
+        public static User FromURL(string url)
+        {
+            var match = Regex.Match(url, @"http://www.elitepvpers.com/forum/members/(\d+)-(\S+).html");
+            if(match.Groups.Count < 3) throw new ParsingFailedException("User could not be exported from the given URL");
+            
+            return new User(match.Groups[2].Value, Convert.ToUInt32(match.Groups[1].Value));
+        }
     }
 }
