0fa77e40d9deb43d5e06b7b91581c11935106d42
Mostey <mostey1337@hotmail.de>
2014-09-13 20:51:28 +0200

    Implemented the base class for HTML parse engines

---
+++ src/epvpapi/HtmlParseEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epvpapi
+{
+    public abstract class HtmlParseEngine<T>
+    {
+        public T Target { get; set; }
+
+        public HtmlParseEngine(T target)
+        {
+            Target = target;
+        }
+
+        public abstract void Execute();
+    }
+}
