c9a45508a992151077fbe051b72a8694607919b2
Shawak <maxi.nussbaum@googlemail.com>
2014-09-21 18:08:34 +0200

    Implemented To<T>() method for IConvertible objects

---
+++ src/epvpapi/Generation/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace epvpapi.Generation
+{
+    class Extensions
+    {
+        public static T To<T>(this string value) where T : IConvertible
+        {
+            return (T)Convert.ChangeType(value, typeof(T));
+        }
+    }
+}
