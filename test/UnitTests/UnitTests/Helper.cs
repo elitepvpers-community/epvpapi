14a1489f02e62e6b5d2cb001673063296c9a9183
Mostey <mostey1337@hotmail.de>
2014-09-22 20:14:55 +0200

    Added UnitTesting project and implemented a helper class for deserializing credentials stored on the local computer. Added a session test for testing the login function

---
+++ test/UnitTests/UnitTests/Helper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    public static class Helper
+    {    
+        public static void AssertException(string msg, Exception exception)
+        {
+            Assert.Fail(msg + "\nException: " + exception.ToString());
+        }
+
+        public struct Credentials
+        {
+            public uint ID { get; set; }
+            public string Name { get; set; }
+            public string MD5Hash { get; set; }
+        }
+
+        public static Credentials LoadTestCredentials()
+        {
+            var credentials = new Credentials();
+
+            var credentialsFile = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "/epvpapi/credentials.txt";
+            using (var fs = new FileStream(credentialsFile, FileMode.Open, FileAccess.Read))
+            {
+                using (var sr = new StreamReader(fs))
+                {
+                    var fileContents = sr.ReadToEnd();
+                    var splittedContents = fileContents.Split(':');
+                    if (splittedContents.Count() == 3)
+                    {
+                        credentials.Name = splittedContents.First();
+                        credentials.ID = Convert.ToUInt32(splittedContents[1]);
+                        credentials.MD5Hash = splittedContents[2];
+                    }
+                }
+            }
+
+            return credentials;
+        }
+    }
+}
---
+++ test/UnitTests/UnitTests/Tests/SessionTest.cs
@@ -0,0 +1,30 @@
+﻿using epvpapi;
+using epvpapi.Connection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Tests
+{
+    [TestClass]
+    public class SessionTest
+    {
+        [TestMethod]
+        public void TestLogin()
+        {
+            try
+            {
+                var testCredentials = Helper.LoadTestCredentials();
+                var session = new ProfileSession<PremiumUser>(new PremiumUser(testCredentials.Name, testCredentials.ID), testCredentials.MD5Hash);
+                Assert.AreEqual(true, session.Valid, "The session is invalid");
+                Assert.IsNotNull(session.SecurityToken, "The session's security token was not detected");
+            }
+            catch (InvalidAuthenticationException exc)
+            {
+                Helper.AssertException("The user was not logged in", exc);
+            }
+            catch (RequestFailedException exc)
+            {
+                Helper.AssertException("A HTTP request failed", exc);
+            }
+        }
+    }
+}
