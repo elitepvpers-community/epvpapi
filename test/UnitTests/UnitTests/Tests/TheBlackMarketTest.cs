117978907d539e526cae6324f15721eb093444a8
Mostey <mostey1337@hotmail.de>
2014-09-23 16:57:44 +0200

    Added user tests (update and search)

--- test/UnitTests/UnitTests/Tests/TheBlackMarketTest.cs
+++ test/UnitTests/UnitTests/Tests/TheBlackMarketTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using epvpapi.Connection;
 using epvpapi.TBM;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -12,27 +13,38 @@ namespace UnitTests.Tests
     [TestClass]
     public class TheBlackMarketTest
     {
-        public void TestGetTreasures(Treasure.Query targetStatus)
+        private void TestGetTreasures(Treasure.Query targetStatus)
         {
-            var treasures = TestEnvironment.Session.ConnectedProfile.GetTreasures(targetStatus, 1, 1);
-            if (treasures.Count == 0)
-                Assert.Fail("No treasures were retrieved");
-
-            if(treasures.Count > 15)
-                Assert.Fail("Too much treasures were retrieved");
-
-            foreach (var treasure in treasures)
+            try
             {
-                Assert.AreNotEqual(0, treasure.ID, "The ID of a treasure was not set");
-                Assert.AreNotEqual(0, treasure.Title.Length, "The title of a treasure was not set");
-                Assert.AreNotEqual(0, treasure.Cost, "The cost of a treasure was not set");
+                var treasures = TestEnvironment.Session.ConnectedProfile.GetTreasures(targetStatus, 1, 1);
+                if (treasures.Count == 0)
+                    Assert.Fail("No treasures were retrieved");
 
-                // The bought request needs to have the seller property set while the selled/listed query does not
-                if (targetStatus == Treasure.Query.Bought)
+                if (treasures.Count > 15)
+                    Assert.Fail("Too much treasures were retrieved");
+
+                foreach (var treasure in treasures)
                 {
-                    Assert.AreNotEqual(0, treasure.Seller.ID);
-                    Assert.AreNotEqual(0, treasure.Seller.Name.Length);
+                    Assert.AreNotEqual(0, treasure.ID, "The ID of a treasure was not set");
+                    Assert.AreNotEqual(0, treasure.Title.Length, "The title of a treasure was not set");
+                    Assert.AreNotEqual(0, treasure.Cost, "The cost of a treasure was not set");
+
+                    // The bought request needs to have the seller property set while the selled/listed query does not
+                    if (targetStatus == Treasure.Query.Bought)
+                    {
+                        Assert.AreNotEqual(0, treasure.Seller.ID);
+                        Assert.AreNotEqual(0, treasure.Seller.Name.Length);
+                    }
                 }
+            }
+            catch (RequestFailedException exc)
+            {
+                AssertExtender.Exception("A HTTP request failed", exc);
+            }
+            catch (InvalidSessionException exc)
+            {
+                AssertExtender.Exception("Session is invalid", exc);
             }
         }
 
