612f6366ec2c208189a30adc8f8071154691fa31
Mostey <mostey1337@hotmail.de>
2014-09-24 15:10:34 +0200

    Implemented Shoutbox.Update test

--- test/UnitTests/UnitTests/Tests/ShoutboxTest.cs
+++ test/UnitTests/UnitTests/Tests/ShoutboxTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using epvpapi;
+using epvpapi.Connection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTests.Tests
@@ -14,20 +15,55 @@ namespace UnitTests.Tests
         [TestMethod]
         public void TestHistory()
         {
-            var globalHistoryShouts = Shoutbox.Global.History(TestEnvironment.Session, 1, 1);
-            Assert.AreNotEqual(0, globalHistoryShouts.Count, "No shouts were pulled from the global channel history");
-
-            // one page contains 15 shouts. If we're specifiying that we want to parse just one page, we shouldn't get more than 15 shouts
-            if(globalHistoryShouts.Count > 15) 
-                Assert.Fail("More than 15 shouts were returned");
-
-            foreach (var shout in globalHistoryShouts)
+            try
             {
-                Assert.AreNotEqual(0, shout.Message.Length);
-                Assert.AreNotEqual(default(DateTime), shout.Time);
-                Assert.AreNotEqual(0, shout.User.Name.Length);
-                Assert.AreNotEqual(0, shout.User.ID);
+
+                var globalHistoryShouts = Shoutbox.Global.History(TestEnvironment.Session, 1, 1);
+                Assert.AreNotEqual(0, globalHistoryShouts.Count, "No shouts were pulled from the global channel history");
+
+                // one page contains 15 shouts. If we're specifiying that we want to parse just one page, we shouldn't get more than 15 shouts
+                if (globalHistoryShouts.Count > 15)
+                    Assert.Fail("More than 15 shouts were parsed");
+
+                foreach (var shout in globalHistoryShouts)
+                {
+                    Assert.AreNotEqual(0, shout.Message.Length, "The message content of a shout was not set");
+                    Assert.AreNotEqual(default(DateTime), shout.Time, "The date and time of a shout was not set");
+                    Assert.AreNotEqual(0, shout.User.Name.Length, "The name of the shout sender was not set");
+                    Assert.AreNotEqual(0, shout.User.ID, "The ID of the shout sender was not set");
+                }
             }
+            catch (RequestFailedException exc)
+            {
+                AssertExtender.Exception("A HTTP request failed", exc);
+            }
+            catch (InvalidSessionException exc)
+            {
+                AssertExtender.Exception("Session is invalid", exc);
+            }  
+        }
+
+        [TestMethod]
+        public void TestUpdate()
+        {
+            try
+            {
+                Shoutbox.Update(TestEnvironment.Session);
+
+                if (Shoutbox.TopChatter.Count > 10)
+                    Assert.Fail("More than 10 top chatters were parsed");
+
+                foreach (var chatter in Shoutbox.TopChatter)
+                    Assert.AreNotEqual(0, chatter.Name.Length, "The name of a top chatter was not set");
+            }
+            catch (RequestFailedException exc)
+            {
+                AssertExtender.Exception("A HTTP request failed", exc);
+            }
+            catch (InvalidSessionException exc)
+            {
+                AssertExtender.Exception("Session is invalid", exc);
+            }  
         }
     }
 }
